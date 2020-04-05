e8c6be04bff6c89f5568c54060f2246067ca9a51
wladweb <wladwebwork@gmail.com>
2020-04-05 20:08:21 +0300

    add  enemy sparite flip due move direction

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,24 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _speed = 2;
 
     private Vector3 _destination;
+    private bool _isFacingRight = true;
+    private bool _isMoveRight;
 
     private void Awake()
     {
         _destination = Random.insideUnitCircle * 100;
+
+        if (_destination.x - transform.position.x < 0)
+        {
+            _isMoveRight = false;
+        }
+        else
+        {
+            _isMoveRight = true;
+        }
+
+        ChangeDirection();
+
         Destroy(gameObject, 10f);
     }
 
@@ -20,5 +34,22 @@ public class Enemy : MonoBehaviour
     public void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, _destination, _speed * Time.deltaTime);
+    }
+
+    public void ChangeDirection()
+    {
+        if (_isMoveRight != _isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
